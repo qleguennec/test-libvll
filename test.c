0c68c28462f50c7ad7a0d965eaae437e4ef38930
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-28 13:40:25 +0000

    ok

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/27 17:27:50 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/27 21:49:05 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/28 13:35:57 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,24 @@
 #define IMPORT(s) 		strcpy(a, s); b = a; l = vll_import(&b, "[],'", 0);
 #define EXPORT(l, v)	vll_export(l, &v, VLL_DEF_DELIM, VLL_EXPORT_DELIM | VLL_EXPORT_STR);
 #define PRINT(l)		printf("%lu\t", l->size); fflush(stdout); vll_print(1, l, VLL_PRINT_NL);
-#define TEST_START(s)	ok = 0; tests = 0; printf("test_%s\n\n", s);
+#define TEST_START(s)	vect_init(&v); ok = 0; tests = 0; printf("\ntest_%s\n", s);
 #define TEST_RESULT		printf("OK: %d/%d\n", ok, tests);
 
 #define TEST_IMP_EXP(s) tests++; \
 	IMPORT(s); \
 	EXPORT(l, v); \
 	if (!strcmp(v.data, s)) \
-		ok++; \
+		{ok++; vll_free(l);} \
 	else \
 		{printf("fail on %s\n", s); PRINT(l); l1 = vll_fmap(l, &_vllsize); PRINT(l1);} \
 	v.used = 0;
+
+#define TEST_SIZE(s, k) tests++; \
+	IMPORT(s); \
+	if (l->size == k) \
+		{ok++; vll_free(l);} \
+	else \
+		{printf("fail on %s\n", s); PRINT(l); l1 = vll_fmap(l, &_vllsize); PRINT(l1);} \
 
 t_vll		*_vllsize(t_vll *l)
 {
@@ -40,8 +47,7 @@ t_vll		*_vllsize(t_vll *l)
 	size = vll_size(l);
 	v = vect_new(NULL, 0);
 	vect_fmt(v, "size = %lu", size);
-	new->head = vll_node_new(v);
-	++l->size;
+	vll_data_add(new, v);
 	return (new);
 }
 
@@ -58,12 +64,31 @@ void		test_imp_exp(void)
 	vect_init(&v);
 	TEST_START("imp_exp");
 
-	TEST_IMP_EXP("['hola']");
-	TEST_IMP_EXP("[]");
-	TEST_IMP_EXP("['vamos ', 'a', '  la  ', 'playa']");
+	TEST_IMP_EXP("['vamos' 'a' 'la' 'playa']");
+	TEST_IMP_EXP("[['vamos' 'a'] ['la'] ['playa']]");
+	TEST_IMP_EXP("[[''] ['hot' 'hot'] ['cold'] ['']]");
+	TEST_IMP_EXP("[['chat'] ['bll'] ['chien' 'dog']]");
 
-	TEST_IMP_EXP("[ [], [], [] ]");
-	//TEST_IMP_EXP("[[], [[['boogie']], ['woogie']]]");
+	free(v.data);
+	TEST_RESULT;
+}
+
+void		test_size(void)
+{
+	t_vll	*l;
+	t_vll	*l1;
+	t_vect	v;
+	char	a[1024];
+	char	*b;
+	int		ok;
+	int		tests;
+
+	TEST_START("size");
+
+	TEST_SIZE("['']", 1);
+	TEST_SIZE("[[''] ['']]", 2);
+	TEST_SIZE("[]", 0);
+	TEST_SIZE("[[''] [''] [''] ['']   ['']]", 5);
 
 	TEST_RESULT;
 }
@@ -71,4 +96,5 @@ void		test_imp_exp(void)
 int			main(void)
 {
 	test_imp_exp();
+	test_size();
 }
